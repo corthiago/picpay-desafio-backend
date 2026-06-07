1c802aeeec7e820ddaf03b9ef504701f471d4392
Thiago Correia <cor.thiago@gmail.com>
2026-06-07 17:33:21 -0300

    feat: create User repository

---
+++ src/main/java/com/thiago/picpaybackendchallenge/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.thiago.picpaybackendchallenge.repository;
+
+import com.thiago.picpaybackendchallenge.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findUserById(UUID id);
+
+    Optional<User> findUserByDocument(String document);
+}
