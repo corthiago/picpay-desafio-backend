c6250cd24b988e556a1056878b0b06a471263b1a
Thiago Correia <cor.thiago@gmail.com>
2026-06-07 17:33:41 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/thiago/picpaybackendchallenge/domain/transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.thiago.picpaybackendchallenge.domain.transaction;
+
+import com.thiago.picpaybackendchallenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
