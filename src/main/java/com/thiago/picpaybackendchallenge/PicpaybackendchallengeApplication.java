2050f08faef4c654d8b55897e6454fec74e328ae
Thiago Correia <cor.thiago@gmail.com>
2026-06-07 17:32:52 -0300

    feat: initial commit

---
+++ src/main/java/com/thiago/picpaybackendchallenge/PicpaybackendchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.picpaybackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaybackendchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaybackendchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/picpaybackendchallenge/PicpaybackendchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.picpaybackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaybackendchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
